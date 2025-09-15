ccd6cf230b5d2174513e20b5eef6185e5ca8ac75
Shawn <shawn.jin@mail.utoronto.ca>
2025-09-15 13:53:47 -0400

    branched and merged

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,11 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        System.out.println(sum(numbers));
+    }
+
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
